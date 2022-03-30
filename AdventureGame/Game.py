12ea1bdc6552958d81efa882555ea826f3bcfa9e
Kozak <wruku1212@gmail.com>
2022-03-30 19:10:48 +0200

    Rozwijanie fabuły gry

--- AdventureGame/Game.py
+++ AdventureGame/Game.py
@@ -40,14 +40,15 @@ def main():
 
     for i, keys in enumerate(weapon_statistics.keys()):
         weapon_statistics[keys] = weapon[0][i]
-
-    list_of_monsters = []
-
-    monsters = dbase.execute('SELECT name, hp, attack, dodge FROM monsters')
-    for monster in monsters:
-        list_of_monsters.append({'name':monster[0],'hp':monster[1],'attack':monster[2],'dodge':monster[3]})
-
     
+    list_of_enemies = []
+
+    enemies = dbase.execute('SELECT name, hp, attack, dodge FROM enemies')
+    for monster in enemies:
+        list_of_enemies.append({'name':monster[0],'hp':monster[1],'attack':monster[2],'dodge':monster[3]})
+    
+    get_enemy = lambda name: [x for x in list_of_enemies if x['name']==name]
+
     #story
     # print('-'*60)
     # print('So you picked your hero, now you can go to conquer new land!')
@@ -67,46 +68,72 @@ def main():
     # time.sleep(1)
     while True:
         # print("")
-        # print('1. Take a contract in which you have to solve riddle in village')
-        # print('2. Take a contract on the troll')
-        # print('')
-        # answer = int(input('Choose: '))
+        # option = answer(["Take a contract in which you have to receive villager's treasures in village", "Take a contract on the troll"])
 
-        # #Riddle in village
-        # if answer==1:
+        #Riddle in village
+        # if option==1:
             rich_villager = Npc('Rich villager')
-        #     rich_villager.window(f"Hello stranger!\nRecently in our village has been strange things.\nIn my house start disappeared treasures.\nI'm pissed off!\nI think that blacksmith steal my things!\nFirst go to him and talk with him.\nIf you catch that the bastard, I will pay you 25 gold.\nWhat do you think? You can do it for me?")
+        #     rich_villager.window(f"Hello stranger!\nRecently in our village has been strange things.\nIn my house start disappeared treasures.\nI'm pissed off!\nI think that blacksmith steal my things!\nFirst go to him and talk with him.\nIf you catch that the bastard, I will pay you 25 gold.\nWhat do you think? You can do it for me?", False)
+
             take = input('(Y/N) ')
             if take.upper()=='Y':
                 #village location
                 village = Room(["Rich villager's house", "Blacksmith's house", "Huntress's house"])
+                blacksmith = Npc('Blacksmith')
+                # gold = dbase.execute(f"SELECT gold FROM main, characters as ch, weapons as w, users as u WHERE id_user = (SELECT id FROM users WHERE name='{dbase.get_name()}')  AND u.name='{dbase.get_name()}' AND main.id_character = ch.id AND id_weapon = w.id ")
+                items = Items(0)
 
+                #flags
                 basement_flag = False
+                blacksmith_fight_flag = False 
+                blacksmith_visit_flag = False
+                end_village = True
 
-                while True:
+                while end_village:
                     print('-'*25)
                     print('Village'.center(25))
                     print('-'*25)
 
-                    village.create_rooms()
-                    print("")
-                    room = int(input('Choose room: '))
-                    print("")
+                    house = village.create_rooms('house')
                     
                     #Rich villager's house
-                    if room==1:
+                    if house==1:
                         while True:
-                            village.name_of_room(room)
+                            village.name_of_room(house)
                             villager_house = Room(['Main room', 'Basement', 'Garden', "Back"])
-                            villager_house.create_rooms()
-                            print("")
-                            doors = int(input('Choose door: '))
-                            print("")
+                            doors = villager_house.create_rooms('door')
                             
                             #main room
                             if doors==1:
-                                rich_villager.window('Why are you still here?!\nGo to blacksmith and receive my treasures!')
-                                input('Press enter: ')
+                                if blacksmith_visit_flag==False:
+                                    rich_villager.window('Why are you still here?!\nGo to blacksmith and receive my treasures!', True)
+                                else:
+                                    pass
+
+                                #after fight with blacksmith 
+                                if blacksmith_fight_flag:
+                                    _ = answer(['I receive your treasures, but unfortunately I had to get rid of thief'])
+
+                                    if _ == 1:
+                                        #choose after stole gold from villager's chest
+                                        if basement_flag:
+                                            rich_villager.window("Give it to me and don't show yourself to me here\nanymore you little thief!", False)
+                                            kill_villager = answer(['Haha! Either you pay me my reward or I kill you', 'Exit'])
+
+                                            if kill_villager==1:
+                                                rich_villager.window("Take it and get the fuck out!", True)
+                                                # dbase.execute(f'INSERT INTO main(gold) VALUES({1})')
+                                                end_village = False
+                                                break
+                                            if kill_villager==2:
+                                                end_village = False
+                                                break
+                                        else:
+                                            rich_villager.window("Haha, everything went my way!\nI mean...\nIt's a shame, but it was a thief anyway.\nHere it your reward.", True)
+                                            print(items.return_items())
+                                            input('as')
+                                            end_village = False
+                                            break
 
                             #basement
                             if doors==2:
@@ -115,15 +142,13 @@ def main():
                                     print('Oh no! You are attacked in the basement by rabid rat!')
                                     print("")
                                     time.sleep(1.5)
-                                    result_of_fight = Combat(character_statistics, weapon_statistics, *[x for x in list_of_monsters if x['name']=='Rat']).fight()
+                                    result_of_fight = Combat(character_statistics, weapon_statistics, *get_enemy('Rat')).fight()
 
                                     if result_of_fight:
                                         print('You defeated rabid rat! You can search basement now.')
                                         villager_house.name_of_room(doors)
-                                        steal_villager_gold = villager_house.items('15 gold', "rich villager's chest")
-                                        basement_flag=True
-                                    else:
-                                        pass
+                                        steal_villager_gold = items.get_items('15 gold', "rich villager's chest")
+                                        basement_flag = True
                                     
                                 else:
                                     if steal_villager_gold:
@@ -131,7 +156,7 @@ def main():
                                         time.sleep(1.5)
                                     else:
                                         villager_house.name_of_room(doors)
-                                        steal_villager_gold = villager_house.items('15 gold', "rich villager's chest")
+                                        steal_villager_gold = items.get_items('15 gold', "in rich villager's chest")
 
 
                             #garden                                    
@@ -142,33 +167,63 @@ def main():
                             #exit
                             if doors==4:
                                 break
-                    if room==2:
+
+                    #blacksmith house
+                    if house==2:
                         while True:
-                            village.name_of_room(room)
-                            blacksmith_house = Room(['Smithy', 'Main room', 'Back']).create_rooms()
-                            print("")
-                            doors = int(input('Choose door: '))
-                            print("")
-
+                            village.name_of_room(house)
+                            blacksmith_house = Room(['Smithy', 'Main room', 'Back'])
+                            doors = blacksmith_house.create_rooms('door')
+                            
+                            #smithy
                             if doors==1:
-                                print('smithy')
+                                if blacksmith_fight_flag==False:
+                                    blacksmith.window("You wanna buy something?\nNot now, I'm working on new project.", True)
 
+                                elif blacksmith_fight_flag:
+                                    print('You see a huge forge that is impressive.')
+                                    time.sleep(1)
+
+                            #main room
                             if doors==2:
-                                print('main room')
+                                if blacksmith==False or blacksmith_fight_flag==False: 
+                                    blacksmith_house.name_of_room(doors)
+                                    items.get_items('precious metals', 'on the table')
+                                    blacksmith.window('Hey! What are you doing here?\nWhy are you hanging around?\nExplain yourself!', False)
+                                    kill_blacksmith = answer(["I'm looking for treasures which you stole rich villager's!", "Not your business. Give me this treasures!"])
 
+                                    if kill_blacksmith==1:
+                                        blacksmith.window('What?! Are you kidding?\nThis dodger has been trying to kick me out of the \nmarket in such ways since last year!\nEhh, I see that he rented another mercenary.', False)
+                                        kill_blacksmith2 = answer([f"Hmm...Interesting, why should I trust you?", "I don't care! Give me this treasures!"])
+
+                                        if kill_blacksmith2==1:
+                                            pass
+
+                                    if kill_blacksmith == 2 or kill_blacksmith2 == 2:
+                                        blacksmith.window('Haha! Fine, stand up for a fight!', True)
+                                        result_of_fight = Combat(character_statistics, weapon_statistics, *get_enemy('Blacksmith')).fight()
+
+                                        if result_of_fight:
+                                            print('You recovered treasures.')
+                                            time.sleep(1)
+                                            blacksmith_fight_flag = True
+                                            blacksmith_visit_flag = True
+
+                                elif blacksmith_fight_flag:
+                                    print('The whole room is covered in blood and you can smell metal...')
+                                    time.sleep(2)
+
+
+                            #exit
                             if doors==3:
                                 break
 
-
-                            break
-
-                    if room==3:
+                    #huntress house
+                    if house==3:
                         while True:
-                            village.name_of_room(room)
-                            huntress_house = Room(['Hunting hut', 'Forest', 'Back']).create_rooms()
-                            print("")
-                            doors = int(input('Choose door: '))
-                            print("")
+                            village.name_of_room(house)
+                            huntress_house = Room(['Hunting hut', 'Forest', 'Back'])
+                            doors = huntress_house.create_rooms('door')
 
                             if doors==1:
                                 print('hunting hut')
@@ -191,3 +246,6 @@ def main():
         
 if __name__ == '__main__':
     main()
+
+
+#dodac golda
